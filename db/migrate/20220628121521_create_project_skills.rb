fdaa90ffc7d856faa2f3935c0e6da7a697a5ba65
Jean Guillet <jean.guillet1@gmail.com>
2022-06-28 14:15:56 +0200

    generating models

---
+++ db/migrate/20220628121521_create_project_skills.rb
@@ -0,0 +1,10 @@
+class CreateProjectSkills < ActiveRecord::Migration[7.0]
+  def change
+    create_table :project_skills do |t|
+      t.references :project, null: false, foreign_key: true
+      t.references :skill, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
