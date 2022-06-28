418a771ee7ad82cf67a8d2ae141144d069905d35
Jean Guillet <jean.guillet1@gmail.com>
2022-06-28 19:02:15 +0200

    adding many_to_many relation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ApplicationRecord
+  has_many :project_skills
+  has_many :skills, through: :project_skills
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+  has_many :project_skills
+  has_many :projects, through: :project_skills
 end
