657d885398b5d3fb1c27efe3b45b2ccd5b93c2cd
Jean Guillet <jean.guillet1@gmail.com>
2022-06-28 18:45:54 +0200

    generating projects, skills and project_skills associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_06_28_123152) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "project_skills", force: :cascade do |t|
-    t.bigint "project_id", null: false
-    t.bigint "skill_id", null: false
+    t.integer "project_id", null: false
+    t.integer "skill_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_project_skills_on_project_id"
