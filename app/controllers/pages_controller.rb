f57d6c1ecc79e718a6c961c9d3ae7105daa5d1a5
Jean Guillet <jean.guillet1@gmail.com>
2022-07-05 19:37:16 +0200

    serving socials content from config/data/socials.yml and @socials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     @projects = Project.all.sample(6)
     @skills = Skill.all.sample(15).shuffle
+    @socials = YAML.safe_load(File.read("#{Rails.root}/config/data/socials.yml"))
   end
 
   def about; end
