e348fd5472db5dc309f17c7f12c10e3c9e8ef08b
Jean Guillet <jean.guillet1@gmail.com>
2022-07-08 16:38:21 +0200

    implementing update-on-scroll navbar controller v1

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  connect() {
+  }
+  updateNavbarPadding() {
+    if (window.scrollY >= 500) {
+      this.element.classList.remove("pt-5")
+      this.element.classList.remove("lg:pt-10")
+      this.element.classList.add("pt-3")
+      this.element.classList.add("lg:pt-5")
+      this.element.classList.add("backdrop-blur")
+    } else {
+      this.element.classList.remove("pt-3")
+      this.element.classList.remove("lg:pt-5")
+      this.element.classList.add("pt-5")
+      this.element.classList.add("lg:pt-10")
+      this.element.classList.remove("backdrop-blur")
+    }
+  }
+}
