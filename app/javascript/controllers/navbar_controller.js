13d047f58306f3aa758cd08531ecb091ff6c10fc
Jean Guillet <jean.guillet1@gmail.com>
2022-07-09 12:06:43 +0200

    added classes to navbar controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -10,13 +10,15 @@ export default class extends Controller {
       this.element.classList.remove("lg:pt-10")
       this.element.classList.add("pt-3")
       this.element.classList.add("lg:pt-5")
-      this.element.classList.add("backdrop-blur")
+      this.element.classList.add("backdrop-blur-lg")
+      this.element.classList.add("bg-white/75")
     } else {
-      this.element.classList.remove("pt-3")
-      this.element.classList.remove("lg:pt-5")
       this.element.classList.add("pt-5")
       this.element.classList.add("lg:pt-10")
-      this.element.classList.remove("backdrop-blur")
+      this.element.classList.remove("pt-3")
+      this.element.classList.remove("lg:pt-5")
+      this.element.classList.remove("backdrop-blur-lg")
+      this.element.classList.remove("bg-white/75")
     }
   }
 }
