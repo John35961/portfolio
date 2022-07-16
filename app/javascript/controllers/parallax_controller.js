5bd19a6704564b10a6b2ef75c43069791934ae64
Jean Guillet <jean.guillet1@gmail.com>
2022-07-16 14:32:17 +0200

    resetting translate3d to 0 for small screens

--- app/javascript/controllers/parallax_controller.js
+++ app/javascript/controllers/parallax_controller.js
@@ -7,7 +7,9 @@ export default class extends Controller {
   connect() {
     const hero = this.heroTarget;
     window.addEventListener('scroll', function (e) {
-      if (window.innerWidth > 1024) {
+      if (window.innerWidth < 1024) {
+        hero.style.transform = `translate3d(0px, 0px, 0px)`;
+      } else {
         let scrolled = window.pageYOffset;
         let rate = scrolled * 0.75;
         hero.style.transform = `translate3d(0px, ${rate}px, 0px)`;
