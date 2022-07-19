7ac4be538265b6149c5bde5d904876fefad15b0c
Jean Guillet <jean.guillet1@gmail.com>
2022-07-19 21:05:00 +0200

    added update logic

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,15 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: %i[show edit]
+  before_action :set_project, only: %i[show edit update]
   skip_before_action :authenticate_admin!, only: %i[index show]
 
   def show; end
 
   def edit; end
+
+  def update
+    @project.update(project_params)
+    redirect_to dashboard_path
+  end
 
   def index
     @projects = Project.all
@@ -14,5 +19,12 @@ class ProjectsController < ApplicationController
 
   def set_project
     @project = Project.friendly.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :project_url, :github_url,
+                                    :banner_url, :release_date, :short_description,
+                                    :long_description, :banner_url_alt_text,
+                                    :is_active, :slug)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,10 @@
 class Project < ApplicationRecord
-  extend FriendlyId
-
   has_many :project_skills
   has_many :skills, through: :project_skills
+
+  extend FriendlyId
+
+  before_save :nil_if_blank
 
   friendly_id :name, use: :slugged
 
@@ -12,5 +14,13 @@ class Project < ApplicationRecord
 
   def self.next(record)
     Project.where('projects.id > ?', record.id).order('created_at ASC').first  || Project.first
+  end
+
+  private
+
+  def nil_if_blank
+    attributes.each_key do |attribute|
+      self[attribute] = nil if self[attribute].blank?
+    end
   end
 end
