eb03e69bad8675e966cb6073ff9c334ff6df4885
Jean Guillet <jean.guillet1@gmail.com>
2022-07-19 17:34:07 +0200

    :authenticate_admin! for dashboard action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :authenticate_admin!, only: %i[dashboard]
+
   def home
     @projects = Project.all.sample(6)
     @skills = Skill.all.sample(15).shuffle
