adabd2dea171887d692f33f527398e1cffbda6a9
Jean Guillet <jean.guillet1@gmail.com>
2022-07-19 17:18:54 +0200

    added after_sign_in_path_for with redirect to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { host: ENV['DOMAIN'] || 'localhost:3000' }
   end
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
