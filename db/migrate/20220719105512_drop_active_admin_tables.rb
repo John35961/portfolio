47a453de2ef8ad79228b5054f4c9f358604f1f4c
Jean Guillet <jean.guillet1@gmail.com>
2022-07-20 18:08:54 +0200

    removed active_admin migrations

--- db/migrate/20220719105512_drop_active_admin_tables.rb
+++
@@ -1,6 +0,0 @@
-class DropActiveAdminTables < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :admin_users
-    drop_table :active_admin_comments
-  end
-end
--- db/migrate/20220719112315_re_drop_active_admin_tables.rb
+++
@@ -1,6 +0,0 @@
-class ReDropActiveAdminTables < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :admin_users
-    drop_table :active_admin_comments
-  end
-end
