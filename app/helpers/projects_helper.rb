15d4e9820300f22c790dff3ff5c7202841691eb2
Jean Guillet <jean.guillet1@gmail.com>
2022-07-20 15:42:29 +0200

    wrote define_banner helper, used in show and card partial

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,4 +2,8 @@ module ProjectsHelper
   def current_pages?(args = {})
     params[:controller] == args[:controller] && args[:actions].include?(params[:action])
   end
+
+  def define_banner(object)
+    object.banner.attached? ? object.banner.url : object.banner_remote_url
+  end
 end
