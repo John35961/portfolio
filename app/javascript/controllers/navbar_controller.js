5d5ebf63874c66afad4be51a0e03455096d4d698
Jean Guillet <jean.guillet1@gmail.com>
2022-07-21 12:43:56 +0200

    updated navbar scroll behaviour

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,24 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="navbar"
 export default class extends Controller {
-  connect() {
+  static targets = ["title", "item"]
+
+  static values = {
+    active: Boolean,
   }
-  updateNavbarPadding() {
+
+  updateNavbar() {
+
+    let title = this.titleTarget;
+    let items = this.itemTargets;
+    let itemIsActive = this.activeValue;
+    let background = this.element;
+
     if (window.scrollY >= 500) {
-      this.element.classList.remove("pt-5")
-      this.element.classList.remove("lg:pt-10")
-      this.element.classList.add("pt-3")
-      this.element.classList.add("lg:pt-5")
-      this.element.classList.add("backdrop-blur-lg")
-      this.element.classList.add("bg-white/75")
+      title.classList.add('text-blue-50')
+      title.classList.remove('text-slate-800')
+      background.classList.add('backdrop-blur-lg', 'bg-slate-900/90')
+      items.forEach((item) => {
+        if (!itemIsActive) {
+          item.classList.add('text-blue-50')
+          item.classList.remove('text-gray-400', 'text-slate-800')
+        }
+      })
     } else {
-      this.element.classList.add("pt-5")
-      this.element.classList.add("lg:pt-10")
-      this.element.classList.remove("pt-3")
-      this.element.classList.remove("lg:pt-5")
-      this.element.classList.remove("backdrop-blur-lg")
-      this.element.classList.remove("bg-white/75")
+      title.classList.remove('text-blue-50')
+      title.classList.add('text-slate-800')
+      background.classList.remove('backdrop-blur-lg', 'bg-slate-900/90')
+      items.forEach(item => {
+        if (!itemIsActive) {
+          item.classList.remove('text-blue-50')
+          item.classList.add('text-gray-400', 'text-slate-800')
+        }
+      });
     }
   }
 }
