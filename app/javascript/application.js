55ce9d0279f19f76e934f8fa6c64817a5db9cce2
Jean Guillet <jean.guillet1@gmail.com>
2022-07-26 13:07:05 +0200

    initializing photoswipe v1

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,17 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-import AOS from 'aos';
+import PhotoSwipeLightbox from 'https://unpkg.com/photoswipe/dist/photoswipe-lightbox.esm.js'
+import AOS from 'aos'
 
 AOS.init({
   once: true,
   offset: 50
 })
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '#gallery',
+  children: 'a',
+  pswpModule: () => import('https://unpkg.com/photoswipe'),
+});
+
+lightbox.init();
