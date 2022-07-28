d23f1c994c3ff70a6ad25318d574c1729d79bdab
Jean Guillet <jean.guillet1@gmail.com>
2022-07-28 15:43:13 +0200

    implemented page transition v1 with barba.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 import "controllers"
 import PhotoSwipeLightbox from 'https://unpkg.com/photoswipe/dist/photoswipe-lightbox.esm.js'
 import AOS from 'aos'
+import barba from '@barba/core'
+import { TimelineMax } from 'gsap'
 
 AOS.init({
   once: true,
@@ -16,3 +18,36 @@ const options = {
 const lightbox = new PhotoSwipeLightbox(options);
 
 lightbox.init();
+
+const wipe = document.querySelector('.wipe-transition')
+
+const TLAnim = new TimelineMax();
+
+function delay(n) {
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  })
+}
+
+barba.init({
+
+  sync: false,
+
+  transitions: [{
+    async leave() {
+      const done = this.async();
+      TLAnim.to(wipe, { bottom: '0%', ease: 'power2.out', duration: 0.55 });
+      await delay(600);
+      done();
+    },
+    enter() {
+      window.scrollTo(0, 0);
+      TLAnim
+        .to(wipe, { bottom: '100%', ease: 'power2.in', duration: 0.55 })
+        .set(wipe, { bottom: '-100%' })
+    }
+  }
+  ]
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "aos", to: "https://ga.jspm.io/npm:aos@2.3.4/dist/aos.js"
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.0.1/dist/stimulus.js"
+pin "@barba/core", to: "https://ga.jspm.io/npm:@barba/core@2.9.7/dist/barba.umd.js"
+pin "gsap", to: "https://ga.jspm.io/npm:gsap@3.10.4/index.js"
