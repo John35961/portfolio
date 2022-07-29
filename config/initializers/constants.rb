460a2a931816fe51ff181dda54b363cdec807e45
Jean Guillet <jean.guillet1@gmail.com>
2022-07-29 17:50:08 +0200

    renamed PERSONAL and personal.yml to ABOUT and about.yml

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,5 +2,5 @@
 
 LEGAL = YAML.load_file(Rails.root.join('config/data/legal.yml'))
 DEFAULT_META = YAML.load_file(Rails.root.join('config/data/default_meta.yml'))
-PERSONAL = YAML.load_file(Rails.root.join('config/data/personal.yml'))
+ABOUT = YAML.load_file(Rails.root.join('config/data/about.yml'))
 SOCIALS = YAML.load_file(Rails.root.join('config/data/socials.yml'))
