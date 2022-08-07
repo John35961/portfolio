7b17d470323d197030076c7f61f847800b3619dd
Jean Guillet <jean.guillet1@gmail.com>
2022-08-07 16:22:37 +0200

    updated with contact@jeanguillet.com

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,13 +4,11 @@ class Contact < MailForm::Base
   attribute :message
   attribute :nickname,  captcha: true
 
-  # Declare the e-mail headers. It accepts anything the mail method
-  # in ActionMailer accepts.
   def headers
     {
-      :subject => "Contact Form Inquiry",
-      :to => "your.email@address.com",
-      :from => %("#{name}" <#{email}>)
+      subject: 'Contact Form Inquiry',
+      to: 'contact@jeanguillet.com',
+      from: %("#{name}" <#{email}>)
     }
   end
 end
