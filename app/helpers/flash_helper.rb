848d4ffcfe6c55f21c2ed112f916d683b9d1b3c7
Jean Guillet <jean.guillet1@gmail.com>
2022-08-09 16:05:56 +0200

    rewrote conditional_flash_classes for better consistency

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,13 +1,13 @@
 module FlashHelper
-  def conditional_classes(type)
-    decorations = {
+  def conditional_flash_classes(type)
+    lookup = {
       'success': 'text-emerald-500 bg-emerald-50',
       'notice': 'text-blue-500 bg-blue-50',
       'error': 'text-red-500 bg-red-50',
       'alert': 'text-yellow-500 bg-yellow-50'
     }
-    decoration = decorations[type.to_sym]
-    decoration += ' fixed right-4 bottom-4' if %w[notice alert].include?(type)
-    decoration
+    classes = lookup[type.to_sym]
+    classes += ' fixed right-4 bottom-4' if %w[notice alert].include?(type)
+    classes
   end
 end
