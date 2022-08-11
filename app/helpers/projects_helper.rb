adc1ab70b6bac3506b2dd13aa3d4b49ebe2eaa4d
Jean Guillet <jean.guillet1@gmail.com>
2022-08-11 12:23:16 +0200

    renamed define_banner to banner_for

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,5 @@
 module ProjectsHelper
-  def define_banner(object)
+  def banner_for(object)
     object.banner.attached? ? object.banner.url.gsub('http://', 'https://') : object.banner_remote_url
   end
 end
