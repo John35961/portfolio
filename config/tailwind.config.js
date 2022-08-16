6c97fc957adcbac95858f6bc74a77001801dc99c
Jean <contact@jeanguillet.com>
2022-08-16 11:18:18 +0200

    replaced yellow-500 with yellow

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,13 +10,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#1e293b', //primary
-        'dark': '#111827', //dark
-        'blue': '#3b82f6', //blue
-        'light-blue': '#eff6ff', //light-blue
-        'green': '#10b981', //green
-        'light-green': '#ecfdf5', //light-green
-        'yellow': '#eab308', //yellow-500
+        'primary': '#1e293b',
+        'dark': '#111827',
+        'blue': '#3b82f6',
+        'light-blue': '#eff6ff',
+        'green': '#10b981',
+        'light-green': '#ecfdf5',
+        'yellow': '#eab308', //yellow
         'light-yellow': '#fefce8', //yellow-50
         'red': '#ef4444', //red-500
         'light-red': '#fef2f2', //red-50
