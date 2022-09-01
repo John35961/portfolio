b5bbc4e839cdf480686dfd4b2e94de66b62d5bb2
Jean <contact@jeanguillet.com>
2022-09-01 17:44:36 +0200

    styled contact form for light and dark mode

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,19 @@
 module ContactsHelper
+  def conditional_form_classes_for(args = {})
+    lookup = {
+      'light': {
+        'label': 'text-primary-900',
+        'notice': 'text-primary-900',
+        'input': 'text-primary-900 placeholder:text-primary-900/25 bg-white border-transparent focus:border-primary-300/25',
+        'button': 'text-primary-900'
+      },
+      'dark': {
+        'label': 'text-light-blue',
+        'notice': 'text-light-blue',
+        'input': 'text-light-blue placeholder:text-primary-300/25 bg-primary-800 border-transparent focus:border-primary-300/25',
+        'button': 'text-light-blue'
+      }
+    }
+    lookup[args[:mode].to_sym][args[:type].to_sym]
+  end
 end
