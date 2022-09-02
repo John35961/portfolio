e99c51faa4e856cdec28d4004b6de2becf98c08e
Jean <contact@jeanguillet.com>
2022-09-02 15:40:39 +0200

    removed form mandatory fields notice

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -3,13 +3,11 @@ module ContactsHelper
     lookup = {
       'light': {
         'label': 'text-primary-900',
-        'notice': 'text-primary-900',
         'input': 'text-primary-900 placeholder:text-primary-900/25 bg-white border-transparent focus:border-primary-300/25',
         'button': 'text-primary-900'
       },
       'dark': {
         'label': 'text-light-blue',
-        'notice': 'text-light-blue',
         'input': 'text-light-blue placeholder:text-primary-300/25 bg-primary-800 border-transparent focus:border-primary-300/25',
         'button': 'text-light-blue'
       }
