b90d73945255a7aeb7f804457b47104ced85f77e
Jean <contact@jeanguillet.com>
2022-09-06 13:12:44 +0200

    added conditional classes for 'notice' type

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -4,14 +4,16 @@ module ContactsHelper
       'light': {
         'label': 'text-primary-900',
         'input': 'text-primary-900 placeholder:text-primary-900/25 bg-white border-transparent focus:border-primary-300/25',
-        'button': 'text-primary-900'
+        'button': 'text-primary-900',
+        'notice': 'text-primary-900 opacity-50'
       },
       'dark': {
         'label': 'text-light-blue',
         'input': 'text-light-blue placeholder:text-primary-300/25 bg-primary-800 border-transparent focus:border-primary-300/25',
-        'button': 'text-light-blue'
+        'button': 'text-light-blue',
+        'notice': 'text-light-blue opacity-50'
       }
     }
     lookup[args[:mode].to_sym][args[:type].to_sym]
-  end
+  end 
 end
