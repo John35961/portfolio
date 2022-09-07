e3df1b826fbe493b141a4be795e354369d74d679
Jean <contact@jeanguillet.com>
2022-09-07 11:29:49 +0200

    initialized access to S3 bucket

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,16 @@
 SitemapGenerator::Sitemap.default_host = ENV['HOST']
 SitemapGenerator::Sitemap.compress = false
+SitemapGenerator::Sitemap.public_path = 'tmp/sitemap'
+
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
+  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  fog_provider: 'AWS',
+  fog_directory: ENV['S3_BUCKET_NAME'],
+  fog_region: ENV['S3_REGION']
+)
+
+SitemapGenerator::Sitemap.sitemaps_host = "https://#{ENV['S3_BUCKET_NAME']}.s3.#{ENV['S3_REGION']}.amazonaws.com"
 
 SitemapGenerator::Sitemap.create do
   add '/contact', changefreq: 'yearly', lastmod: Time.now, priority: 0.3
