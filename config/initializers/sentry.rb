41e5ff9413f19071aac400b7db057e08bda37836
Jean <contact@jeanguillet.com>
2025-03-08 18:30:07 +0100

    sentry in production only

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.dsn = ENV['SENTRY_DSN']
-  config.enable_tracing = true
-  config.send_default_pii = true
+if Rails.env.production?
+  Sentry.init do |config|
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.dsn = ENV['SENTRY_DSN']
+    config.enable_tracing = true
+    config.send_default_pii = true
+  end
 end
