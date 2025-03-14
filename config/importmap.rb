50c7affc651929e5f13fc84f1bd14079ed5d240e
Jean <contact@jeanguillet.com>
2025-03-14 22:35:04 +0100

    fixed photoswipe import source to jsdelivr

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,7 @@ pin_all_from 'app/javascript/plugins', under: 'plugins'
 
 pin 'aos', to: 'https://ga.jspm.io/npm:aos@2.3.4/dist/aos.js'
 pin '@hotwired/stimulus', to: 'https://ga.jspm.io/npm:@hotwired/stimulus@3.0.1/dist/stimulus.js'
-pin 'photoswipe', to: 'https://unpkg.com/photoswipe/dist/photoswipe-lightbox.esm.js'
+pin 'photoswipe', to: 'https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.min.js'
 pin 'gsap', to: 'https://ga.jspm.io/npm:gsap@3.11.1/index.js'
 pin 'gsap/ScrollToPlugin', to: 'https://ga.jspm.io/npm:gsap@3.11.1/ScrollToPlugin.js'
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
