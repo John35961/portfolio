938269a5eb24165cfa84dba2b3ab4125289a768c
Jean <contact@jeanguillet.com>
2025-03-15 01:00:43 +0100

    logic for maintenance mode in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_admin!
+  before_action :display_maintenance_mode
+
   def default_url_options
     { host: ENV['WWW'] || 'localhost:3000' }
   end
@@ -7,5 +10,18 @@ class ApplicationController < ActionController::Base
     dashboard_path
   end
 
-  before_action :authenticate_admin!
+  def display_maintenance_mode
+    redirect_to maintenance_path if subject_to_maintenance_mode? && Setting.instance.maintenance_mode?
+  end
+
+  private
+
+  def subject_to_maintenance_mode?
+    return false if devise_controller? || admin_signed_in?
+    return false if controller_name == "maintenance" && action_name == "show"
+    return false if controller_name == "admin" && action_name == "dashboard"
+    return false if controller_name == "projects" && action_name.in?(["new", "create", "edit", "update", "destroy"])
+
+    true
+  end
 end
