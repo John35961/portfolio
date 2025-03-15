20de5e2a9549096da06a0f2851e988363a959ad6
Jean <contact@jeanguillet.com>
2025-03-15 01:24:45 +0100

    removed target

--- app/javascript/controllers/toggle_maintenance_mode_controller.js
+++ app/javascript/controllers/toggle_maintenance_mode_controller.js
@@ -2,8 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-maintenance-mode"
 export default class extends Controller {
-  static targets = ["checkbox"]
-
   onSubmit(event) {
     event.preventDefault();
     const url = this.element.action;
